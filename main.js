b177252eff6b5c08816fddc0e838cc4264dbe93b
Robert <work@robmail.pw>
2023-05-16 01:27:43 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   data.json
    	new file:   index.html
    	new file:   main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            let startDate = new Date('2023-05-16');
+            let today = new Date();
+            let dayNumber = Math.ceil((today - startDate) / (1000 * 60 * 60 * 24)) + 1;
+
+            if (dayNumber >= 1 && dayNumber <= 30) {
+                let workoutData = data['day' + dayNumber];
+
+                document.getElementById('workout').innerHTML = workoutData['workout'].join('<br>');
+                document.getElementById('meal').textContent = workoutData['meal'];
+                document.getElementById('quote').textContent = workoutData['quote'];
+
+                if (dayNumber % 7 !== 0) {
+                    let weekNumber = Math.ceil(dayNumber / 7);
+                    document.getElementById('grocery').innerHTML = data['groceries']['week' + weekNumber].join('<br>');
+                    document.getElementById('grocery').style.display = 'block';
+                } else {
+                    document.getElementById('grocery').style.display = 'none';
+                }
+            } else {
+                document.getElementById('workout').textContent = 'Workout plan is not available for today.';
+                document.getElementById('meal').textContent = '';
+                document.getElementById('quote').textContent = '';
+                document.getElementById('grocery').style.display = 'none';
+            }
+        })
+        .catch(error => console.error(error));
+});
