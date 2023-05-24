7367684933d901f79c171a6bc467bd7bdb13a629
Robert <work@robmail.pw>
2023-05-23 21:32:46 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             let startDate = new Date('2023-05-23');
             let today = new Date();
-            let dayNumber = Math.ceil((today - startDate) / (1000 * 60 * 60 * 23)) ;
+            let dayNumber = Math.ceil((today - startDate) / (1000 * 60 * 60 * 24)) ;
 
             if (dayNumber >= 1 && dayNumber <= 30) {
                 let workoutData = data['day' + dayNumber];
